53fdeac799d238260d7c9a2560483818b68902c8
Millyannah Gidayi <millyannahi@gmail.com>
2024-12-09 11:38:49 +0300

    church

---
+++ church/src/Components/home.jsx
@@ -0,0 +1,18 @@
+
+function Home() {
+    return(
+        
+        <div>
+            <div>Nangoga P.A.G </div>
+            <div>
+            <a href="#">Home</a>
+            <a href="#">About Us</a>
+            <a href="#">Events</a>
+            <a href="#">Our Team</a>
+            <a href="#">Visit Us</a>
+            <a href="#">Our Programs</a>
+            </div>
+        </div>
+    )
+}
+export default Home;
