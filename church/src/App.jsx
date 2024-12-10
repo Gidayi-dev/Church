58d0d78c3e041497630789f804c4f7c9dbef3b57
Millyannah Gidayi <millyannahi@gmail.com>
2024-12-10 21:41:11 +0300

    service complete

--- church/src/App.jsx
+++ church/src/App.jsx
@@ -1,11 +1,13 @@
 import Home from "./Components/home";
 import About from "./Components/about";
+import Service from "./Components/service";
 
 function App() {
   return (
     <div>
       <Home />
       <About />
+      <Service/>
     </div>
   );
 }
